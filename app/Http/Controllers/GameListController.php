0056e5de6e452f44eeec12ea8d53a31f66d5be0c
SeyyaS <yasin.bellidinc@gmail.com>
2021-12-19 15:00:43 +0300

    Game List transactions

--- app/Http/Controllers/GameListController.php
+++ app/Http/Controllers/GameListController.php
@@ -2,18 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-
 class GameListController extends Controller
 {
     public function index()
     {
-
-        $api = Http::get('https://www.cheapshark.com/api/1.0/games?title=batman&limit=60&exact=0');
-        $includingBatmanLists = json_decode($api->body());
-        // dd($includingBatmanLists);
         return view('gamelist');
     }
-
 }
