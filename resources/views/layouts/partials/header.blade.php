b3f32395317bae951c2e64767949650bad078f63
SeyyaS <yasin.bellidinc@gmail.com>
2021-12-19 23:12:03 +0300

    Header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,4 +1,20 @@
 @include('layouts.partials.head')
+
 <body>
-	<div id="page">
-    <header class="header menu_fixed"></header>
+    <div id="page">
+        <header class="header menu_fixed">
+            <nav id="menu" class="main-menu">
+                <ul>
+                    <li>
+                        <span>
+                            <a href="{{ route('index') }}"><b>Oyun Listesi</b></a>
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            <a href="{{ route('bracketControlForm') }}"><b>Parantez Kontrol</b></a>
+                        </span>
+                    </li>
+                </ul>
+            </nav>
+        </header>
