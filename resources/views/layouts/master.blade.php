ffc9c1ecf5f46eb3ef91c753e9beab567e0821ef
SeyyaS <yasin.bellidinc@gmail.com>
2021-12-19 14:51:51 +0300

    initial commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 
-@include('layouts.partials.header')
-@yield('styles')
+    @include('layouts.partials.header')
+    @yield('styles')
 
 	<!-- /header -->
 	<main>
@@ -14,24 +14,6 @@
 	<!--/footer-->
 	</div>
 	<!-- page -->
-
-	<!-- Sign In Popup -->
-	<div id="sign-in-dialog" class="zoom-anim-dialog mfp-hide">
-		<div class="small-dialog-header">
-			<h3>@lang('system_translate.front.sign_in')</h3>
-		</div>
-		<div class="sign-in-wrapper">
-			<nav>
-				<div class="nav nav-tabs" id="nav-tab" role="tablist">
-					<a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-owner" role="tab" aria-controls="nav-home" aria-selected="true">@lang('system_translate.front.company_owner')</a>
-					<a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-guest" role="tab" aria-controls="nav-profile" aria-selected="false">@lang('system_translate.front.visitor')</a>
-				</div>
-			</nav>
-		</div>
-</div>
-	<!-- /Sign In Popup -->
-
-	<!-- <div id="toTop"></div>< Back to top button -->
 
 	<!-- COMMON SCRIPTS -->
    @include('layouts.partials.javascript')
--- resources/views/layouts/partials/javascript.blade.php
+++ resources/views/layouts/partials/javascript.blade.php
@@ -4,3 +4,4 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9">
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js">
 <script src="/js/modernizr_slider.js">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
